dc8fd36158b64f61a92c8abf88700fcbafa02ce1
Adam Lipert <34799600+Ortieez@users.noreply.github.com>
2022-12-12 14:08:51 +0100

    Add more variations of operations (PS Pane uciteli uz toho tam je dostatek ;D)

--- index.php
+++ index.php
@@ -3,6 +3,10 @@ if (filter_input(INPUT_GET, "operation") == "" || filter_input(INPUT_GET, "numbe
     echo "Please try urls: <br>";
     echo "<a href='?operation=add&numbers=1,1'>?operation=add&numbers=1,1</a> <br><br>";
     echo "<a href='?operation=sub&numbers=35,10,10'>?operation=sub&numbers=35,10,10</a> <br><br>";
+    echo "<a href='?operation=mul&numbers=35,10,10'>?operation=mul&numbers=35,10,10</a> <br><br>";
+    echo "<a href='?operation=div&numbers=35,10'>?operation=div&numbers=35,10</a> <br><br>";
+    echo "<a href='?operation=mod&numbers=35,10'>?operation=mod&numbers=35,10</a> <br><br>";
+    echo "<a href='?operation=sqrt&numbers=100'>?operation=sqrt&numbers=100</a> <br><br>";
 
     die("Not enough parameters. Or wrong parameters entered <br>");
 }
@@ -10,18 +14,47 @@ if (filter_input(INPUT_GET, "operation") == "" || filter_input(INPUT_GET, "numbe
 $oper = filter_input(INPUT_GET, "operation");
 $nums = explode(',', filter_input(INPUT_GET, "numbers"));
 
-if (count($nums) <= 1) {
-    echo json_encode(["report" => "Not enough numbers", "statuscode"=> ""]);
-    die();
-}
-
 $res = [];
+
+header('Content-Type: application/json; charset=utf-8');
 switch ($oper) {
     case 'add':
+        if (count($nums) <= 1) {
+            echo json_encode(["report" => "Not enough numbers", "statuscode"=> "418"]);
+            die();
+        }
         $res = add($nums);
         break;
     case 'sub':
+        if (count($nums) <= 1) {
+            echo json_encode(["report" => "Not enough numbers", "statuscode"=> "418"]);
+            die();
+        }
         $res = sub($nums);
+        break;
+    case 'mul':
+        if (count($nums) <= 1) {
+            echo json_encode(["report" => "Not enough numbers", "statuscode"=> "418"]);
+            die();
+        }
+        $res = mul($nums);
+        break;
+    case 'div':
+        if (count($nums) <= 1) {
+            echo json_encode(["report" => "Not enough numbers", "statuscode"=> "418"]);
+            die();
+        }
+        $res = div($nums);
+        break;
+    case 'mod':
+        if (count($nums) <= 1) {
+            echo json_encode(["report" => "Not enough numbers", "statuscode"=> "418"]);
+            die();
+        }
+        $res = mod($nums);
+        break;
+    case 'sqrt':
+        $res = sqrtArr($nums);
         break;
     default:
 }
@@ -54,6 +87,91 @@ function sub(array $nums)
             continue;
         }
         $sum -= $num;
+    }
+    return ["report" => "200 OK", "result" => $sum];
+}
+
+function mul(array $nums) {
+    $sum = 0;
+    $tempRes = isNumberInArray($nums);
+    if ($tempRes !== True)
+        return $tempRes;
+
+    $firstNum = True;
+    foreach ($nums as $num) {
+        if ($firstNum) {
+            $sum = $num;
+            $firstNum = False;
+            continue;
+        }
+        $sum *= $num;
+    }
+    return ["report" => "200 OK", "result" => $sum];
+}
+
+function div(array $nums) {
+    $sum = 0;
+    $tempRes = isNumberInArray($nums);
+    if ($tempRes !== True)
+        return $tempRes;
+
+    $firstNum = True;
+    foreach ($nums as $num) {
+        if ($firstNum) {
+            $sum = $num;
+            $firstNum = False;
+            continue;
+        }
+        if ($num != 0) {
+            $sum /= $num;
+        } else {
+            return ["report" => "Cannot divide by zero"];
+        }
+    }
+    return ["report" => "200 OK", "result" => $sum];
+}
+
+function mod(array $nums) {
+    $sum = 0;
+    $tempRes = isNumberInArray($nums);
+    if ($tempRes !== True)
+        return $tempRes;
+
+    $firstNum = True;
+    foreach ($nums as $num) {
+        if ($firstNum) {
+            $sum = $num;
+            $firstNum = False;
+            continue;
+        }
+        if ($num != 0) {
+            $sum %= $num;
+        } else {
+            return ["report" => "Cannot modulo by zero"];
+        }
+    }
+    return ["report" => "200 OK", "result" => $sum];
+}
+
+function sqrtArr(array $nums) {
+    $sum = 0;
+    $tempRes = isNumberInArray($nums);
+    if ($tempRes !== True)
+        return $tempRes;
+
+    $firstNum = True;
+    if (count($nums) > 1) {
+        foreach ($nums as $num) {
+            if ($firstNum) {
+                $sum = $num*$num;
+                $firstNum = False;
+                continue;
+            }
+            $sum += $num*$num;
+        }
+    } else {
+        $sum = $nums[0]*$nums[0];
+        return ["report" => "200 OK", "result" => $sum];
     }
     return ["report" => "200 OK", "result" => $sum];
 }
